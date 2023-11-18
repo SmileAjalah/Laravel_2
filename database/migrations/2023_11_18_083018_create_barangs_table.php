f93c769b3320f2ba57706e9d6c0d08d2bd6bc026
unknown <69Happy.Ajalah69@gmail.com>
2023-11-18 16:31:56 +0700

    first

---
+++ database/migrations/2023_11_18_083018_create_barangs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_barang');
+            $table->string('nama_barang');
+            $table->string('tanggal_pembelian');
+            $table->string('type_barang');
+            $table->string('harga_barang');
+            $table->string('jumlah');
+            $table->string('keterangan');
+            $table->string('kode_investaris');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangs');
+    }
+};
---
+++ resources/views/barang.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+        integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <title>Barang</title>
+
+</head>
+
+<body>
+
+    <div class="container-fluid mt-3">
+        <table class="table table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Kode Barang</th>
+                    <th scope="col">Nama Barang</th>
+                    <th scope="col">Tanggal Pembelian</th>
+                    <th scope="col">Type Barang</th>
+                    <th scope="col">Harga Barang</th>
+                    <th scope="col">Jumlah</th>
+                    <th scope="col">Keterangan</th>
+                    <th scope="col">Kode Investaris</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($barang as $b)
+                    <tr>
+                        <th scope="row">{{ $b->kode_barang }}</th>
+                        <td>{{ $b->nama_barang }}</td>
+                        <td>{{ $b->tanggal_pembelian }}</td>
+                        <td>{{ $b->type_barang }}</td>
+                        <td>{{ $b->harga_barang }}</td>
+                        <td>{{ $b->jumlah }}</td>
+                        <td>{{ $b->keterangan }}</td>
+                        <td>{{ $b->kode_investaris }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
